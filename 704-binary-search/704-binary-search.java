034fdd006bf5040abcd881c69c4f4da44856c230
mariamrady92019 <55314273+mariamrady92019@users.noreply.github.com>
2022-05-20 17:38:18 +0200

    Time: 0 ms (100.00%), Space: 54.5 MB (38.10%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,30 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int mid=0;
-        if(nums.length>=2){
-         mid = (nums.length/2)-1;
-        }
-        
-        if(nums[mid]==target)
-            return mid;
-        else if(target<nums[mid]){
-            
-          for(int i = 0 ; i<mid;i++){
-              if(nums[i]==target){
-                  return i;
-              }
-          }  
-            
-        }else{
-            
-          for(int j =mid ; j<nums.length;j++){
-              if(nums[j]==target){
-                  return j;
-              }
-          }   
-        }
-        
-        return -1 ;
-        
+       int mid = 0;
+        int left=0;
+        int right = nums.length-1;
+       while(left<=right){
+           mid =left+ (right - left) / 2;
+           if(nums[mid]==target)
+               return mid;
+           
+           else if(nums[mid]>target)
+               right = mid-1;
+           else left = mid +1;
+           
+       }
+        return -1;
     }
 }
